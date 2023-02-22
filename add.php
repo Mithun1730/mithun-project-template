6d063830ae01cc833578da1263bde0f88792eba5
Mithun A <mitchellmithun@gmail.com>
2023-02-22 14:38:40 +0530

    modified 2:38

--- add.php
+++ add.php
@@ -2,10 +2,11 @@
 include "connnect.php";
 
 extract($_POST);
-if(isset($_POST['NameSend']) && isset($_POST['TitleSend']) 
-&& isset($_POST['AddressSend']) && isset($_POST['MobileSend']) 
-&& isset($_POST['EmailSend']) && isset($_POST['SalarySend'])){
-  $sql= "INSERT INTO `employees`( `employee_name`, `title`, `employee_address`, `employee_email`, `emoloyee_mobile`, `basic_salary`) VALUES ('$NameSend','$TitleSend','$AddressSend','$EmailSend','$MobileSend','$SalarySend')";
+
+if(isset($_POST['name']) && isset($_POST['title']) 
+&& isset($_POST['address']) && isset($_POST['mobile']) 
+&& isset($_POST['email']) && isset($_POST['salary'])){
+  $sql= "INSERT INTO `employees`( `employee_name`, `title`, `employee_address`, `employee_email`, `emoloyee_mobile`, `basic_salary`) VALUES ('$name','$title','$address','$email','$mobile','$salary')";
 }
 $result=mysqli_query($sql,$conn);
 if($result){
--- index.php
+++ index.php
@@ -8,34 +8,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="button.css">
-  <link rel="stylesheet" href="dist/js/jquery.min.js">
-  <script>
-  function adduser(){
-    var Name=$('#name').val();
-    var Title=$('#title').val();
-    var Address=$('#address').val();
-    var Email=$('#email').val();
-    var Salary=$('#salary').val();
-    var Mobile=$('#mobile').val();
-
-    $.ajax({
-      url:"add.php",
-      type:"post",
-      data: {
-        NameSend:Name,
-        TitleSend:Title,
-        AddressSend:Address,
-        EmailSend:Email,
-        SalarySend:Salary,
-        MobileSend:Mobile
-      },
-      success:function(data,status){
-        console.log(status);
-      }
-    });
-  }
-</script>
-
 </head>
 <body>
   
@@ -55,61 +27,80 @@
                     Add new employee
                   </a>
                   </div>
-
+                  
                   <div class="modal fade" id="modal-report" tabindex="-1" aria-labelledby="modal-report" aria-hidden="true">
                      <div class="modal-dialog">
                         <div class="modal-content">
                           <div class="modal-header">
                               <h1 class="modal-title fs-5" id="exampleModalLabel">Employee Details</h1>
-                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              <button type="button" id="adduser" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                           </div>
                     
                           
                           <div class="modal-body">
-                          <div class="mb-3">
+                          <form role="form">
+                          <div class="form-group">
                               <label class="form-label">Name</label>
                               <input type="text" id="name" class="form-control" name="example-text-input" placeholder="Input placeholder">
-                            </div>
-
-                            <div class="mb-3">
-                              <label class="form-label">Title<span class="form-label-description">56/100</span></label>
-                              <textarea class="form-control" id="title" name="example-textarea-input" rows="6" placeholder="Content..">Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed! We shall say 'Ni' again to you, if you do not appease us. I'm not a witch. I'm not a witch. Camelot!</textarea>
-                            </div>
-
-                            <div class="mb-3">
-                              <label class="form-label">Address <span class="form-label-description">56/100</span></label>
-                              <textarea class="form-control" id="address" name="example-textarea-input" rows="6" placeholder="Content..">Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed! We shall say 'Ni' again to you, if you do not appease us. I'm not a witch. I'm not a witch. Camelot!</textarea>
-                            </div>
-
-                            <div class="mb-3">
-                            <label class="form-label">Email address</label>
-                            <input type="email" id="email" class="form-control" placeholder="Email">
                           </div>
 
-                          <div class="mb-3">
+                          <div class="form-group">
+                              <label class="form-label">Title<span class="form-label-description">56/100</span></label>
+                              <textarea class="form-control" id="title" name="example-textarea-input" rows="6" placeholder="Content.."></textarea>
+                            </div>
+
+                            <div class="form-group">
+                              <label class="form-label">Address <span class="form-label-description">56/100</span></label>
+                              <textarea class="form-control" id="address" name="example-textarea-input" rows="6" placeholder="Content.."></textarea>
+                            </div>
+
+                            <div class="form-group">
+                              <label class="form-label">Email address</label>
+                              <input type="email" id="email" class="form-control" placeholder="example@gmail.com">
+                            </div>
+
+                            <div class="form-group">
                               <label class="form-label">Basic Salary</label>
                               <input type="tel" id="salary" class="form-control" autocomplete="off">
                             </div>
           
-                          <div class="mb-3">
+                            <div class="form-group">
                               <label class="form-label">Phone number</label>
                               <input type="tel" id="mobile" class="form-control" autocomplete="off">
                             </div>
-                  </div>
+                            </form>
+                        </div>
                 <div class="modal-footer">
-                <button type="button" onclick="adduser()" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <a href="#" class="btn btn-primary ms-auto" data-bs-dismiss="modal">
-              <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
-              Create new employee
-            </a>
+                  <button type="button"  class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <a href="#" class="btn btn-primary ms-auto" data-bs-dismiss="modal">
+                  <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
+                    Create new employee
+                  </a>
                 
                 </div>
                 </div>
                 </div>
                 </div>
 
+                <script>
+                    $(document).ready(function(){
+                    $("#adduser").click(function(){
+                    var name = $('#name').val();
+                    var title = $('#title').val();
+                    var address = $('#address').val();
+                    var email = $('#email').val();
+                    var salary = $('#salary').val();
+                    var mobile = $('#mobile').val();
 
+                      $.ajax({
+                      url: "add.php",
+                      method:"POST",
+                      data:{name:name,title:title,address:address,email:email,salary:salary,mobile:mobile},
+                      });
+                    });
+                  }); 
+                </script>
 
 
                 <table class="table table-vcenter table-mobile-md card-table">
